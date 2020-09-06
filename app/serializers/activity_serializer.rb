386df1f91bf2e7fbcce33554a8d4b81f62297e4b
Nathan <chung.nathanh@gmail.com>
2020-09-05 23:18:17 -0700

    adds associations to serializers. thanks noa!

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,3 +1,5 @@
 class ActivitySerializer < ActiveModel::Serializer
   attributes :id, :name, :address, :city, :state, :zipcode, :description, :image, :category
+  has_many :comments
+  has_many :users, through: :comments
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :address, :city, :state, :zipcode, :image
+  has_many :comments
+  has_many :user_activities
 end
