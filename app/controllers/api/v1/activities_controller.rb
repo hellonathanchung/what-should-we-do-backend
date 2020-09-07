10174a84100d0e7749289279d791e0f5e184ba33
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-09-06 19:30:33 -0700

    Changes controllers files, update CRUD to activivties

---
+++ app/controllers/api/v1/activities_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::ActivitiesController < ApplicationController
+  def index
+    activities = Activity.all
+    render json: activities
+  end
+
+  def show
+    activity = Activity.find_by(id: params[:id])
+    if activity
+      render json: activity
+    else
+      render json: { message: 'This ID does not exist' }
+    end
+  end
+
+  def create
+    activity = Activity.new(avctivity_params)
+
+    if activity.save
+      render json: activity
+    else
+      render json: { error: 'Something went wrong' }
+    end
+  end
+
+  def update
+    activity = Activity.find_by(id: params[:id])
+
+    if activity.save
+      render json: activity
+    else
+      render json: { error: 'Something went wrong' }
+    end
+  end
+
+  def destroy
+    activity = Activity.find_by(id: params[:id])
+    activity.destroy
+
+    render json: { message: 'deleted' }
+  end
+
+  private
+
+  def avctivity_params
+    params.require(:avctivity).permit(:id, :name, :address, :city, :state, :zipcode, :description, :image, :category)
+  end
+end
