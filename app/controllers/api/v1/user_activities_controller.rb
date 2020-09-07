39bfc1904a1b0b959a5e3745924ea95bcf28d499
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-09-07 10:07:59 -0700

    add carete and destroy method to UserActivity

--- app/controllers/api/v1/user_activities_controller.rb
+++ app/controllers/api/v1/user_activities_controller.rb
@@ -7,4 +7,27 @@ class Api::V1::UserActivitiesController < ApplicationController
   def show
     user_activity = UserActivity.find_by(id: params[:id])
   end
+
+  def create
+    user_activity = UserActivity.new(user_activity_params)
+
+    if user_activity.save
+      render json: user_activity
+    else
+      render json: { error: 'Something went wrong' }
+    end
+  end
+
+  def destroy
+    user_activity = UserActivity.find_by(id: params[:id])
+    user_activity.destroy
+
+    render json: { message: 'deleted' }
+  end
+
+  private
+
+  def user_activity_params
+    params.require(:user_activity).permit(:id, user_id:, activity_id:)
+  end
 end
