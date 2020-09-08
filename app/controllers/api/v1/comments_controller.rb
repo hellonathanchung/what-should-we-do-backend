14a2cbd7dc670ae82fd7e8d499c08733c52f9cac
Taci Shlosberg <tshlosberg@gmail.com>
2020-09-08 08:48:26 -0700

    Added show, create, destroy and private params to comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -3,4 +3,35 @@ class Api::V1::CommentsController < ApplicationController
     comments = Comment.all
     render json: comments
   end
+
+  def show
+    comment = Comment.find(params[:id])
+    render json: comment
+  end
+
+  def create 
+    comment = Comment.new(comment_params)
+
+    if comment.save
+      render json: comment
+    else
+      render json: {error: 'Something went wrong'}
+    end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    render json: {message: 'Successfully deleted comment'}
+  end
+
+  private   
+
+  def comment_params
+    params.require(:comment).permit(:id, :activity_id, :content)
+  end
 end
+
+
+
+
